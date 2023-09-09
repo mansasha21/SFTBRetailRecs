7b6fd5602527b64022727263341840e95cb3dff8
Александр Суржиков <aleksandr.e.surzhikov@sbermarket.ru>
2023-09-09 13:31:45 +0300

    add reqs.txt and workflow.py

---
+++ src/workflow.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from typing import Dict
+
+import polars as pl
+from typer import Option, Typer
+
+
+cli = Typer()
+
+
+@cli.command()
+def load():
+    pass
+
+
+@cli.command()
+def train_candidate_models():
+    pass
+
+@cli.command()
+def inference_candidates():
+    pass
+
+
+@cli.command()
+def candidates_join_features():
+    pass
+
+
+@cli.command()
+def train_ranker():
+    pass
+
+@cli.command()
+def make_recommendations():
+    pass
+
+@cli.command()
+def evaluate_common_metrics():
+    pass
+
+@cli.command()
+def evaluate_candidates_metrics():
+    pass
+
+if __name__ == "__main__":
+    cli()
