3137636a25adb4ab4c2ace04d38b96fcbac32170
lllchak <mediumchak@yandex.ru>
2023-09-10 06:50:52 +0300

    added docker

--- src/recs.sh
+++ src/recs.sh
@@ -1,16 +1,18 @@
-export TRAIN_DATA_PATH=../data/supermarket_train.tsv
-export VAL_DATA_PATH=../data/supermarket_val.tsv
-export INFERENCE_DATA_PATH=../data/supermarket_val.tsv
-export TARGET_PATH="../data/supermarket_val_target.tsv"
+#!/bin/bash
+
+export TRAIN_DATA_PATH="$1"
+export VAL_DATA_PATH="$2"
+export INFERENCE_DATA_PATH="$3"
+export TARGET_PATH="$4"
 
 
 ###################
 
-PYTHONPATH=. python workflow.py load --train-data-path $TRAIN_DATA_PATH --val-data-path $VAL_DATA_PATH
-PYTHONPATH=. python workflow.py train-candidate-models
-PYTHONPATH=. python workflow.py inference-candidates --inference-data-path $INFERENCE_DATA_PATH
-PYTHONPATH=. python workflow.py train-ranker --train-data-path $TRAIN_DATA_PATH --val-data-path $VAL_DATA_PATH
-PYTHONPATH=. python workflow.py make-recommendations --val-data-path $VAL_DATA_PATH
-PYTHONPATH=. python workflow.py evaluate-common-metrics --target-path $TARGET_PATH
+PYTHONPATH=. python3 workflow.py load --train-data-path $TRAIN_DATA_PATH --val-data-path $VAL_DATA_PATH
+PYTHONPATH=. python3 workflow.py train-candidate-models
+PYTHONPATH=. python3 workflow.py inference-candidates --inference-data-path $INFERENCE_DATA_PATH
+PYTHONPATH=. python3 workflow.py train-ranker --train-data-path $TRAIN_DATA_PATH --val-data-path $VAL_DATA_PATH
+PYTHONPATH=. python3 workflow.py make-recommendations --val-data-path $VAL_DATA_PATH
+PYTHONPATH=. python3 workflow.py evaluate-common-metrics --target-path $TARGET_PATH
 
-PYTHONPATH=. python main/prod_workflow.py evaluate-candidates-metrics 
+PYTHONPATH=. python3 main/prod_workflow.py evaluate-candidates-metrics
